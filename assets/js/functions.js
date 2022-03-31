b0be570e0da8c6e0a3d70817a276f71b6f178584
DifferentWorks <DifferentWorks@users.noreply.github.com>
2022-03-31 14:49:10 +0200

    Update on fonctionality
    New Intro, start and refresh links, links disappearing, table of contents, conclusion appearing and bibliography styling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,41 +1,40 @@
 $(document).ready(function() {
 
-// functions go here
-
 var i=0;
 
+$('a.start').on('click', function(e){
+  $('.intro').addClass('hidden');
+  $('.extra-links').removeClass('hidden');
+});
+
 $('a.link-opens-chapter').on('click', function(e){
+
+  var cards = $( 'section.card[id^="chapter-"]' ).not( '.hidden' );
+  cards.each( function( index, card ) {
+  	var disabledlinks = $( this ).find( 'a.link-opens-chapter' );
+  	disabledlinks.addClass( 'hidden' );
+  } );
 
   $target = $(this).attr('href');
   if ( $('section'+$target+'').hasClass('hidden') ) {
     i++;
-	  $('section'+$target+'').removeClass('hidden');
-	  $('section'+$target+'').css({'order': i});
+
+    $('section'+$target+'').removeClass('hidden');
+	$('section'+$target+'').css({'order': i});
+
+    if (i==4){
+      $("#conclusion").removeClass('hidden');
+      $('.extra-links').addClass('hidden');
+    }
   }
+
+  var cards = $( 'section.card[id^="chapter-"]' ).not( '.hidden' );
+  cards.each( function( index, card ) {
+  	var disabledlinks = $( 'a.link-opens-chapter[href="#' + card.id + '"]' );
+  	disabledlinks.addClass( 'hidden' );
+  } );
+
   console.log(i);
 });
 
-$('#toBottom').on('click', function() {
-  $("html, body").animate({ scrollTop: $(document).height()-$(window).height() });
 });
-
-$('button.collapsible').on('click', function(e){
-  if ( $('.content').hasClass('hidden') ) {
-	  $('.content').removeClass('hidden');
-    $(this).addClass('active');
-  } else {
-    $('.content').addClass('hidden');
-    $(this).removeClass('active');
-  }
-});
-
-});
-
-
-
-// let scrollToBottom = document.querySelector("#toBottom");
-// let pageBottom = document.querySelector("#pageBottom");
-//
-// scrollToBottom.addEventListener("click", function() {
-//   pageBottom.scrollIntoView();
-// });
